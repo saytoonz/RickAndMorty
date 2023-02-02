d461bf2cbb52d8005d89a75693f16805469d13fc
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-02 20:33:04 +0000

    BottomNav icons set up

--- RickAndMorty/Controllers/Core/RMTabBarController.swift
+++ RickAndMorty/Controllers/Core/RMTabBarController.swift
@@ -29,7 +29,23 @@ final class RMTabBarController: UITabBarController {
         let nav2 = UINavigationController(rootViewController: locationsVC)
         let nav3 = UINavigationController(rootViewController: episodesVC)
         let nav4 = UINavigationController(rootViewController: settingsVC)
-       
+        
+        nav1.tabBarItem = UITabBarItem(
+            title: "Characters",
+            image: UIImage(systemName: "person"),
+            tag: 1)
+        nav2.tabBarItem = UITabBarItem(
+            title: "Locations",
+            image: UIImage(systemName: "globe"),
+            tag: 2)
+        nav3.tabBarItem = UITabBarItem(
+            title: "Episodes",
+            image: UIImage(systemName: "tv"),
+            tag: 3)
+        nav4.tabBarItem = UITabBarItem(
+            title: "Settings",
+            image: UIImage(systemName: "gear"),
+            tag: 4)
         
         let navs = [nav1, nav2, nav3, nav4]
         
