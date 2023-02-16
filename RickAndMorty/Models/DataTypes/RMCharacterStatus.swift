6b7edf9ea640e01d11c1598e6e83e3caf5b51d25
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-16 23:55:06 +0000

    Update RMCharacterStatus.swift

--- RickAndMorty/Models/DataTypes/RMCharacterStatus.swift
+++ RickAndMorty/Models/DataTypes/RMCharacterStatus.swift
@@ -11,5 +11,14 @@ enum RMCharacterStatus: String, Codable {
     case alive = "Alive"
     case dead = "Dead"
     case `unknown` = "unknown"
+    
+    var text:String {
+        switch self {
+        case .alive, .dead:
+            return rawValue
+        case .unknown:
+         return   "Unknown"
+        }
+    }
 }
  
