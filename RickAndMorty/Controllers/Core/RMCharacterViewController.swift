224b893c84e8ea14beb4cefe8bcf76cf72cf998a
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-16 23:55:03 +0000

    Update RMCharacterViewController.swift

--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 /// Controller to show and shearch for Characters
-final class RMCharacterViewController: UIViewController {
-    private let characterListView = CharacterListView()
+final class RMCharacterViewController: UIViewController, RMCharacterListViewDelegate {
+    
+    private let characterListView = RMCharacterListView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +20,7 @@ final class RMCharacterViewController: UIViewController {
     }
     
     private func setUpView(){
+        characterListView.delegate = self
         view.addSubview(characterListView)
         NSLayoutConstraint.activate([
             characterListView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
@@ -26,5 +28,16 @@ final class RMCharacterViewController: UIViewController {
             characterListView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
             characterListView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
         ])
+    }
+    
+    
+    // MARK: - RMCharacterListViewDelegate
+    func rmCharacterListView(_ characterListView: RMCharacterListView, didSelectCharacter character: RMCharacter) {
+        //Open detail controller for that character
+        
+        let viewModel = RMCharacterDetailViewViewModel(character: character)
+        let detailVC = RMCharacterDetailViewController(viewModel: viewModel)
+        detailVC.navigationItem.largeTitleDisplayMode = .never
+        navigationController?.pushViewController(detailVC, animated: true)
     }
 }
