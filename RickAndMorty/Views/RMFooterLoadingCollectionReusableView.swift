3de7ea8aec3100feb8661c19baeb9bcf65756018
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-17 22:02:27 +0000

    Update RMFooterLoadingCollectionReusableView.swift

--- RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
+++ RickAndMorty/Views/RMFooterLoadingCollectionReusableView.swift
@@ -10,15 +10,34 @@ import UIKit
 final class RMFooterLoadingCollectionReusableView: UICollectionReusableView {
     static let identifier = "RMFooterLoadingCollectionReusableView"
     
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .medium)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .systemBlue     }
+        backgroundColor = .systemBackground
+        addSubview(spinner)
+        addConstraints()
+    }
     
     required init?(coder: NSCoder) {
         fatalError("Unsupported")
     }
     
     private  func addConstraints() {
-        
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
+    
+    public func startAnimating(){
+        spinner.startAnimating()
     }
 }
