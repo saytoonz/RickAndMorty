a7d21a2c9efd120faf4e4112112f007f5634e4a5
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-18 10:22:53 +0000

    Update RMCharacterDetailView.swift

--- RickAndMorty/Views/RMCharacterDetailView.swift
+++ RickAndMorty/Views/RMCharacterDetailView.swift
@@ -7,7 +7,16 @@
 
 import UIKit
 
+/// View for single character info
 final class RMCharacterDetailView: UIView {
 
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .systemPurple
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Unsupported")
+    }
 }
