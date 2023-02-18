3563013d7bc57d8861cc5a360f275f8ed958ee72
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-18 10:22:50 +0000

    Update RMCharacterDetailViewViewModel.swift

--- RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterDetailViewViewModel.swift
@@ -14,8 +14,31 @@ final class RMCharacterDetailViewViewModel {
         self.character = character
     }
     
+    private var requestUrl:URL? {
+        return URL(string: character.url)
+    }
     
     public var title: String{
         return character.name.uppercased()
+    }
+    
+    func fetchCharactersData(){
+        
+        guard let url = requestUrl,
+              let request = RMRequest(url: url)else{
+             return
+        }
+        
+        RMService.shared.execute(
+            request,
+            expecting: RMCharacter.self){ result in
+                switch result {
+                case .success(let data):
+                    print(String(describing: data))
+                case .failure(let error):
+                    print(String(describing: error))
+                }
+                
+            }
     }
 }
