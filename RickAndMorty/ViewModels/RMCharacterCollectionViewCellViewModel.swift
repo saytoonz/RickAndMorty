5212035a0a42ac1560bc4767852198ecc5cba95a
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-18 09:04:19 +0000

    Update RMCharacterCollectionViewCellViewModel.swift

--- RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -37,18 +37,7 @@ private let characterImageUrl: URL?
             return
         }
         
-        let request = URLRequest(url: url)
-        let task = URLSession.shared.dataTask(with: request){ data, _ , error in
-            guard let data = data, error == nil else{
-                completion(.failure(error ?? URLError(.badServerResponse)))
-                return
-            }
-            
-            completion(.success(data))
-            
-        }
-        
-        task.resume()
+        RMImageLoader.share.downloadImage(url, completion: completion)
     }
     
     
