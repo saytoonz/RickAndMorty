28985b8aa0ff6d024a4a3868c4b2e77382ba9e6b
saytoonz09 <35128520+saytoonz@users.noreply.github.com>
2023-02-18 10:22:45 +0000

    Update RMCharacterDetailViewController.swift

--- RickAndMorty/Controllers/Others/RMCharacterDetailViewController.swift
+++ RickAndMorty/Controllers/Others/RMCharacterDetailViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 final class RMCharacterDetailViewController: UIViewController {
     
     private let viewModel: RMCharacterDetailViewViewModel
+     
+    private let detailView = RMCharacterDetailView()
+    
     
     //MARK: - init
+    
     init(viewModel: RMCharacterDetailViewViewModel) {
         self.viewModel = viewModel
          super.init(nibName: nil,bundle: nil)
@@ -22,13 +26,39 @@ final class RMCharacterDetailViewController: UIViewController {
         fatalError("Unsupported")
     }
 
-    
     //MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         title = viewModel.title
+        view.addSubview(detailView)
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(didTapShare))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .action,
+            target: self,
+            action: #selector(didTapShare)
+        )
+        
+        addContraints()
+        viewModel.fetchCharactersData()
+    }
+    
+   
+    
+    @objc
+    private func didTapShare(){
+        //Share ooo share, share character info
+    }
+    
+    private func addContraints() {
+        NSLayoutConstraint.activate([
+            detailView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            detailView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            detailView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            detailView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
     }
 
 }
